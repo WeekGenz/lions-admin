2eafe587e91772e4de2e214954d39269b7819738
RICO <ricoyou99@gmail.com>
2022-11-01 20:36:07 +0700

    fix broken images preview

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,5 @@
 module.exports = [
   "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
   {
     name: "strapi::security",
     config: {
@@ -35,4 +26,12 @@ module.exports = [
       },
     },
   },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
